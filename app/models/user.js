237d61716c502be1ab87ab0c0c70ba1b51664b4d
Justin Bitely <jbitely@gmail.com>
2016-02-11 22:58:09 -0500

    Refactored to mongodb.

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,54 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
+var bluebird = require('bluebird');
+var mongoose = require('mongoose');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
+var userSchema = mongoose.Schema({
+  username : {
+    type: String,
+    required: true,
+    index: {unique : true}
   },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
+  password: {
+    type: String,
+    required: true
   }
 });
 
+var User = mongoose.model("User", userSchema);
+
+// var User = db.Model.extend({
+//   tableName: 'users',
+//   hasTimestamps: true,
+//   initialize: function(){
+//     this.on('creating', this.hashPassword);
+//   },
+User.comparePassword = function(candidatePassword, savedPassword, cb) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
+    if(err){
+      cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+
+userSchema.pre("save", function(next){
+  var cipher = bluebird.promisify(brcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash){
+      this.password = hash;
+      next();
+  });
+});
+
 module.exports = User;
+
+// hashPassword: function(){
+//     var cipher = Promise.promisify(bcrypt.hash);
+//     return cipher(this.get('password'), null, null).bind(this)
+//       .then(function(hash) {
+//         this.set('password', hash);
+//       });
+//   }
+// });
+
